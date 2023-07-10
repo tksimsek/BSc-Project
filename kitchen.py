1a1283f6cc36734a77944a705ccc6ba0b8a81239
tksimsek <tksimsek.robotics@gmail.com>
2023-07-10 13:10:05 +0200

    initial commit

---
+++ kitchen.py
@@ -0,0 +1,32 @@
+import numpy as np
+# np.set_printoptions(threshold = np.inf)
+
+# size = 2
+# thickness = 2
+
+# height = 10
+# width = 10
+
+# mask = np.zeros((width, height), dtype=int)
+# print(mask)
+# print("\n")
+
+# for y in range(height):
+#     for x in range(width):
+#         if (width/2 - size) <= x < (width/2 + size):
+#             if (height/2 - size) <= y < (height/2 + size):
+#                 mask[x, y] = 1
+        
+        
+
+
+# file = open("MyFile.txt", "w")
+# file.truncate(0)
+# content = str(mask)
+# file.write(content)
+# file.close()
+
+# print(mask)
+
+
+
